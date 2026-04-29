5c819bd26b56609181a76e8663333760201f57fc
elovmeister <elojad@LML-5CG53230KT.lantm.intra.local>
2026-04-29 11:33:58 +0200

    Ny start på komponent och tester.

---
+++ packages/components/src/utils/icon-registry.d.ts
@@ -0,0 +1,8 @@
+export declare const iconRegistry: {
+    readonly 'arrow-right': "<path d=\"M5 12h14\"/><path d=\"m12 5 7 7-7 7\"/>";
+    readonly check: "<path d=\"M20 6 9 17l-5-5\"/>";
+    readonly 'chevron-down': "<path d=\"m6 9 6 6 6-6\"/>";
+    readonly loader: "<path d=\"M21 12a9 9 0 1 1-6.219-8.56\"/>";
+    readonly x: "<path d=\"M18 6 6 18\"/><path d=\"m6 6 12 12\"/>";
+};
+export type IconName = keyof typeof iconRegistry;
