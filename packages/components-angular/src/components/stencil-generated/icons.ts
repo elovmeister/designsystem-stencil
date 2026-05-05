10c11f3506517370f75b22c528658fa8a19ba80c
elovmeister <elojad@LML-5CG53230KT.lantm.intra.local>
2026-05-05 14:53:22 +0200

    Icon fix v2

--- packages/components-angular/src/components/stencil-generated/icons.ts
+++ packages/components-angular/src/components/stencil-generated/icons.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZo
 
 import { ProxyCmp } from './angular-component-lib/utils';
 
-import type { Components } from '@lm-prototype-stencil/icons/dist/components';
+import type { Components } from '@lm-prototype-stencil/icons/dist/types/components';
 
 import { defineCustomElement as defineLmPrototypeStencilIcon } from '@lm-prototype-stencil/icons/dist/components/lm-prototype-stencil-icon.js';
 @ProxyCmp({
